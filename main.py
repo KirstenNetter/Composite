6dca76ddf7d2101ae2f34948a112814a9f064ba1
KINETTER <kirsten.netter@btc-ag.com>
2025-01-07 13:45:32 +0100

    added: Methoden count_items und calculate_size

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from src.file import File
 from src.folder import Folder
 
 if __name__ == "__main__":
-    # Wurzelverzeichnis
+   
     root = Folder("root")
 
-    # Dateien und Ordner
-    file1 = File("file1.txt")
-    file2 = File("file2.txt")
+    file1 = File("file1.txt", 10)  # 10 KB
+    file2 = File("file2.txt", 20)  # 20 KB
     subfolder1 = Folder("subfolder1")
-    subfolder1.add(File("file3.txt"))
-    subfolder1.add(File("file4.txt"))
+    subfolder1.add(File("file3.txt", 5))   # 5 KB
+    subfolder1.add(File("file4.txt", 15))  # 15 KB
 
     subfolder2 = Folder("subfolder2")
-    subfolder2.add(File("file5.txt"))
+    subfolder2.add(File("file5.txt", 50))  # 50 KB
 
     # Aufbau der Hierarchie
     root.add(file1)
@@ -21,5 +20,8 @@ if __name__ == "__main__":
     root.add(subfolder1)
     root.add(subfolder2)
 
-    # Anzeige
     root.display()
+
+    print("\nTotal items in root:", root.count_items())
+
+    print("Total size of files in root:", root.calculate_size(), "KB")
--- src/folder.py
+++ src/folder.py
@@ -16,3 +16,15 @@ class Folder(FileSystemNode):
         print(' ' * indent + f"Folder: {self.name}")
         for child in self.children:
             child.display(indent + 2)
+
+    def count_items(self):
+        total_count = 0
+        for child in self.children:
+            total_count += child.count_items()
+        return total_count
+
+    def calculate_size(self):
+        total_size = 0
+        for child in self.children:
+            total_size += child.calculate_size()
+        return total_size
