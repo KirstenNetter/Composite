2dd8736b1a23e91bc9d30bf2ebc05ce0b36e0eca
KINETTER <kirsten.netter@btc-ag.com>
2025-01-07 13:54:50 +0100

    Refactoring

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
     root = Folder("root")
 
     file1 = File("file1.txt", 10)  # 10 KB
-    file2 = File("file2.txt", 20)  # 20 KB
+    file2 = File("file2.txt", 20)   
+    file3 = File("file3.txt", 5)
+    file4 = File("file4.txt", 15)
+    file5 = File("file5.txt", 50)
     subfolder1 = Folder("subfolder1")
-    subfolder1.add(File("file3.txt", 5))   # 5 KB
-    subfolder1.add(File("file4.txt", 15))  # 15 KB
-
     subfolder2 = Folder("subfolder2")
-    subfolder2.add(File("file5.txt", 50))  # 50 KB
 
     # Aufbau der Hierarchie
     root.add(file1)
@@ -20,8 +19,12 @@ if __name__ == "__main__":
     root.add(subfolder1)
     root.add(subfolder2)
 
+    subfolder1.add(file3)   
+    subfolder1.add(file4) 
+  
+    subfolder2.add(file5)  
+
     root.display()
 
     print("\nTotal items in root:", root.count_items())
-
     print("Total size of files in root:", root.calculate_size(), "KB")
